43a318d16ccaedae5bfcd5104f2a356fb8afcc17
xbaldiq <iqbaldhiaa@gmail.com>
2024-02-16 08:44:35 +0700

    Initial commit

---
+++ src/components/ExploreContainer.tsx
@@ -0,0 +1,23 @@
+import "./ExploreContainer.css";
+
+interface ContainerProps {}
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+  return (
+    <div id="container">
+      <strong>Ready to create an app?</strong>
+      <p>
+        Start with Ionic{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://ionicframework.com/docs/components"
+        >
+          UI <p className="italic underline">Components</p>
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default ExploreContainer;
