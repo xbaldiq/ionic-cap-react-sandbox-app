71d36f5c3f08077bffca122042cc2140cbb3b08c
xbaldiq <iqbaldhiaa@gmail.com>
2024-02-20 10:01:40 +0700

    feat: geolocation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,8 +8,22 @@ import {
 import ExploreContainer from "../components/ExploreContainer";
 import "./Home.css";
 import Map from "../components/Map";
+import { Geolocation } from "@capacitor/geolocation";
+import { useEffect } from "react";
 
 const Home: React.FC = () => {
+  const printCurrentPosition = async () => {
+    const coordinates = await Geolocation.getCurrentPosition();
+
+    console.log("Current position:", coordinates);
+  };
+
+  useEffect(() => {
+    (async () => {
+      await printCurrentPosition();
+    })();
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
